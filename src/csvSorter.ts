26ec4eab5b07080b07fd709c07d251172a0a86f4
antmic <antoni.michera@gmail.com>
2024-01-14 21:19:05 +0100

    prepared winning_words.csv

--- src/csvSorter.ts
+++ src/csvSorter.ts
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
+const path = './winning_words.csv';
+const outputName = 'sorted_winning_words.csv';
+
 // Read the CSV file and parse it into an array of strings
 const data: string[] = [];
-fs.createReadStream('./dictionary.csv')
+fs.createReadStream(path)
 	.pipe(csv())
 	.on('data', (row: string) => {
 		// Explicitly type 'row' as string
@@ -12,6 +15,22 @@ fs.createReadStream('./dictionary.csv')
 
 		// Remove invisible characters
 		value = value.replace(/\r?\n|\r/g, '');
+
+		// Remove leading and trailing whitespace
+		value = value.trim();
+
+		// Remove empty strings
+		if (value === '') {
+			return;
+		}
+
+		// Remove values that contain '-'
+		if (value.includes('-')) {
+			return;
+		}
+
+		// Change all values to lowercase
+		value = value.toLowerCase();
 
 		data.push(value);
 	})
@@ -23,5 +42,5 @@ fs.createReadStream('./dictionary.csv')
 
 		// Write the sorted array back to a new CSV file
 		const output = data.join('\n');
-		fs.writeFileSync('sorted_dictionary.csv', output);
+		fs.writeFileSync(outputName, output);
 	});
